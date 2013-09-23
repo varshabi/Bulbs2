c61428d6078e69ce08f741c0a97614c56166504e
Your Name Here <shahrouz@varshabi.com>
2013-09-23 13:36:46 -0400

    First Push

---
+++ images/changeimage.js
@@ -0,0 +1,221 @@
+function changeImage1()
+{
+
+if (document.bulb1.src=='file:///J:/darpan/html%20bulb/images/01.png'){
+document.bulb1.src='images/01-10.png';
+}
+
+else if (document.bulb1.src=='file:///J:/darpan/html%20bulb/images/01-10.png'){
+document.bulb1.src='images/01.png';
+}
+}
+
+function changeImage12()
+{
+
+if (document.bulb12.src=='file:///J:/darpan/html%20bulb/images/01.png'){
+document.bulb12.src='images/01-10.png';
+}
+
+else if (document.bulb12.src=='file:///J:/darpan/html%20bulb/images/01-10.png'){
+document.bulb12.src='images/01.png';
+}
+}
+
+
+function changeImage2()
+{
+
+if (document.bulb2.src=='file:///J:/darpan/html%20bulb/images/02.png'){
+document.bulb2.src='images/02-10.png';
+}
+
+else if (document.bulb2.src=='file:///J:/darpan/html%20bulb/images/02-10.png'){
+document.bulb2.src='images/02.png';
+}
+}
+
+function changeImage22()
+{
+
+if (document.bulb22.src=='file:///J:/darpan/html%20bulb/images/02.png'){
+document.bulb22.src='images/02-10.png';
+}
+
+else if (document.bulb22.src=='file:///J:/darpan/html%20bulb/images/02-10.png'){
+document.bulb22.src='images/02.png';
+}
+}
+
+function changeImage23()
+{
+
+if (document.bulb23.src=='file:///J:/darpan/html%20bulb/images/02.png'){
+document.bulb23.src='images/02-10.png';
+}
+
+else if (document.bulb23.src=='file:///J:/darpan/html%20bulb/images/02-10.png'){
+document.bulb23.src='images/02.png';
+}
+}
+
+
+function changeImage24()
+{
+
+if (document.bulb24.src=='file:///J:/darpan/html%20bulb/images/02.png'){
+document.bulb24.src='images/02-10.png';
+}
+
+else if (document.bulb24.src=='file:///J:/darpan/html%20bulb/images/02-10.png'){
+document.bulb24.src='images/02.png';
+}
+}
+
+function changeImage25()
+{
+
+if (document.bulb25.src=='file:///J:/darpan/html%20bulb/images/02.png'){
+document.bulb25.src='images/02-10.png';
+}
+
+else if (document.bulb25.src=='file:///J:/darpan/html%20bulb/images/02-10.png'){
+document.bulb25.src='images/02.png';
+}
+}
+
+
+
+
+function changeImage3()
+{
+if (document.bulb3.src=='file:///J:/darpan/html%20bulb/images/03.png'){
+document.bulb3.src='images/03-10.png';
+}
+
+else if (document.bulb3.src=='file:///J:/darpan/html%20bulb/images/03-10.png'){
+document.bulb3.src='images/03.png';
+}
+}
+
+function changeImage32()
+{
+if (document.bulb32.src=='file:///J:/darpan/html%20bulb/images/03.png'){
+document.bulb32.src='images/03-10.png';
+}
+
+else if (document.bulb32.src=='file:///J:/darpan/html%20bulb/images/03-10.png'){
+document.bulb32.src='images/03.png';
+}
+}
+
+function changeImage33()
+{
+if (document.bulb33.src=='file:///J:/darpan/html%20bulb/images/03.png'){
+document.bulb33.src='images/03-10.png';
+}
+
+else if (document.bulb33.src=='file:///J:/darpan/html%20bulb/images/03-10.png'){
+document.bulb33.src='images/03.png';
+}
+}
+
+function changeImage34()
+{
+if (document.bulb34.src=='file:///J:/darpan/html%20bulb/images/03.png'){
+document.bulb34.src='images/03-10.png';
+}
+
+else if (document.bulb34.src=='file:///J:/darpan/html%20bulb/images/03-10.png'){
+document.bulb34.src='images/03.png';
+}
+}
+
+function changeImage35()
+{
+if (document.bulb35.src=='file:///J:/darpan/html%20bulb/images/03.png'){
+document.bulb35.src='images/03-10.png';
+}
+
+else if (document.bulb35.src=='file:///J:/darpan/html%20bulb/images/03-10.png'){
+document.bulb35.src='images/03.png';
+}
+}
+
+
+function changeImage4()
+{
+if (document.bulb4.src=='file:///J:/darpan/html%20bulb/images/04.png'){
+document.bulb4.src='images/04-10.png';
+}
+
+else if (document.bulb4.src=='file:///J:/darpan/html%20bulb/images/04-10.png'){
+document.bulb4.src='images/04.png';
+}
+}
+
+function changeImage5()
+{
+if (document.bulb5.src=='file:///J:/darpan/html%20bulb/images/05.png'){
+document.bulb5.src='images/05-10.png';
+}
+
+else if (document.bulb5.src=='file:///J:/darpan/html%20bulb/images/05-10.png'){
+document.bulb5.src='images/05.png';
+}
+}
+
+function changeImage52()
+{
+if (document.bulb52.src=='file:///J:/darpan/html%20bulb/images/05.png'){
+document.bulb52.src='images/05-10.png';
+}
+
+else if (document.bulb52.src=='file:///J:/darpan/html%20bulb/images/05-10.png'){
+document.bulb52.src='images/05.png';
+}
+}
+
+function changeImage53()
+{
+if (document.bulb53.src=='file:///J:/darpan/html%20bulb/images/05.png'){
+document.bulb53.src='images/05-10.png';
+}
+
+else if (document.bulb53.src=='file:///J:/darpan/html%20bulb/images/05-10.png'){
+document.bulb53.src='images/05.png';
+}
+}
+
+function changeImage54()
+{
+if (document.bulb54.src=='file:///J:/darpan/html%20bulb/images/05.png'){
+document.bulb54.src='images/05-10.png';
+}
+
+else if (document.bulb54.src=='file:///J:/darpan/html%20bulb/images/05-10.png'){
+document.bulb54.src='images/05.png';
+}
+}
+
+function changeImage55()
+{
+if (document.bulb55.src=='file:///J:/darpan/html%20bulb/images/05.png'){
+document.bulb55.src='images/05-10.png';
+}
+
+else if (document.bulb55.src=='file:///J:/darpan/html%20bulb/images/05-10.png'){
+document.bulb55.src='images/05.png';
+}
+}
+
+function changeImage6()
+{
+if (document.bulb6.src=='file:///J:/darpan/html%20bulb/images/06.png'){
+//document.bulb6.src='images/06-10.png';
+}
+
+else if (document.bulb6.src=='file:///J:/darpan/html%20bulb/images/06-10.png'){
+//document.bulb6.src='images/06.png';
+}
+}
---
+++ images/drop.js
@@ -0,0 +1,79 @@
+
+     $(document).ready(function(){
+  $(function(){
+	$("#bulb3").animate({bottom: '-=280'}, 2000,function(){
+	
+		$("#bulb2").animate({bottom: '-=290'}, 2000,function(){
+			
+			$("#bulb5").animate({bottom: '-=460'}, 2000, function(){
+			
+				$("#bulb1").animate({bottom: '-=260'}, 2000, function(){
+				
+					$("#bulb32").animate({bottom: '-=300'}, 2000, function(){
+					
+						$("#bulb33").animate({bottom: '-=260'}, 2000, function(){
+						
+							$("#bulb52").animate({bottom: '-=460'}, 2000, function(){
+							
+								$("#bulb22").animate({bottom: '-=290'}, 2000, function(){
+								
+									$("#bulb23").animate({bottom: '-=260'}, 2000, function(){
+									
+										$("#bulb6").animate({bottom: '-=520'}, 2000,function(){
+										
+											$("#bulb24").animate({bottom: '-=260'}, 2000, function(){
+											
+												$("#bulb53").animate({bottom: '-=360'}, 2000, function(){
+												
+													$("#bulb25").animate({bottom: '-=220'}, 2000, function(){
+													
+														$("#bulb54").animate({bottom: '-=460'}, 2000, function(){
+															
+															$("#bulb34").animate({bottom: '-=240'}, 2000, function(){
+															
+																$("#bulb12").animate({bottom: '-=260'}, 2000, function(){
+																
+																	$("#bulb55").animate({bottom: '-=460'}, 2000, function(){
+																	
+																		$("#bulb35").animate({bottom: '-=240'}, 2000,function(){
+																		
+																			$("#bulb4").animate({bottom: '-=330'}, 2000);
+																		
+																		});
+																	
+																	});
+																
+																});
+															
+															});
+														
+														});
+													
+													});
+												
+												});
+											
+											});
+										
+										});
+									
+									});
+								
+								});
+							
+							});
+						
+						});
+					
+					});
+				
+				});
+			
+			});
+			
+		});
+		
+	});	
+	
+  });
+});
